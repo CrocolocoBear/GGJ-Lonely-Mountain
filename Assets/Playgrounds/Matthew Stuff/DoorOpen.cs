b1de00c2394e77a49cd8a84a409132325fb139d6
Kazkel <8571947+Kazkel@users.noreply.github.com>
2022-01-30 14:24:06 +0000

    doors repositioned for pivots
    make sure the rotation is based on pivots, local at the top of the scene editor

---
+++ Assets/Playgrounds/Matthew Stuff/DoorOpen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    //when the player gets close, rotate 90 degrees, then stop checking  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
